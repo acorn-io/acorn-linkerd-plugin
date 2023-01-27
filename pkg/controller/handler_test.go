353984a4f1e4c89c2508d3fd1cbc71240ea7e7d3
Daishan Peng <daishan@acorn.io>
2023-01-27 01:05:41 -0700

    Add tests, configure ci to run test
    Signed-off-by: Daishan Peng <daishan@acorn.io>

---
+++ pkg/controller/handler_test.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/acorn-io/acorn-linkerd-plugin/pkg/scheme"
+	"github.com/acorn-io/baaah/pkg/router/tester"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestHandler_AddAnnotations(t *testing.T) {
+	harness, input, err := tester.FromDir(scheme.Scheme, "testdata/annotations")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := tester.NewRequest(t, harness.Scheme, input, harness.Existing...)
+
+	if err := AddAnnotations(req, nil); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "enabled", input.GetAnnotations()[serviceMeshAnnotation])
+}
+
+func TestHandler_KillLinkerdSidecar(t *testing.T) {
+	harness, input, err := tester.FromDir(scheme.Scheme, "testdata/killsidecar")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := tester.NewRequest(t, harness.Scheme, input, harness.Existing...)
+
+	h := Handler{
+		client:     fake.NewSimpleClientset(input),
+		debugImage: "foo",
+	}
+
+	if err := h.KillLinkerdSidecar(req, nil); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := corev1.EphemeralContainer{
+		TargetContainerName: proxySidecarContainerName,
+		EphemeralContainerCommon: corev1.EphemeralContainerCommon{
+			Name:  "shutdown-sidecar",
+			Image: "foo",
+			Command: []string{
+				"curl",
+				"-X",
+				"POST",
+				"http://localhost:4191/shutdown",
+			},
+		},
+	}
+	assert.Equal(t, expected, input.(*corev1.Pod).Spec.EphemeralContainers[0])
+}
+
+func TestHandler_AddLinkerdServer(t *testing.T) {
+	tester.DefaultTest(t, scheme.Scheme, "testdata/server", AddLinkerdServer)
+}
+
+func TestHandler_AddAuthorizationPolicy(t *testing.T) {
+	h := Handler{
+		clusterDomain: "cluster.local",
+	}
+	tester.DefaultTest(t, scheme.Scheme, "testdata/authorization-policy", h.AddAuthorizationPolicy)
+}
